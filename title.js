77e4212fb5296a9964bb299661d313b035b1e3e9
Simas2006 <sjuknelis@gmail.com>
2018-06-04 20:47:25 -0400

    Add title image + remove useless style.css

--- title.js
+++ title.js
@@ -1,6 +1,12 @@
 var titleMode = 0;
 
 function renderTitle() {
+  ctx.globalAlpha = 1;
   ctx.fillStyle = "white";
+  ctx.fillRect(0,0,canvas.width,canvas.height);
+  var image = document.getElementById("image");
+  ctx.drawImage(image,0,0,canvas.width,canvas.height);
+  ctx.globalAlpha = 0.5;
+  ctx.fillStyle = "#000099";
   ctx.fillRect(0,0,canvas.width,canvas.height);
 }
