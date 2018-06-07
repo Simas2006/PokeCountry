505e8b16cb146b69929b9e84f017d5c6fb7781b0
Simas2006 <sjuknelis@gmail.com>
2018-06-06 21:49:56 -0400

    Remove all the background stuff (titleMode 1)

--- title.js
+++ title.js
@@ -1,4 +1,4 @@
-var titleMode = 0;
+var titleMode = 1;
 var titleCountryX = 0;
 var titleMoving = 0;
 var titleAskFinal = false;
@@ -8,11 +8,13 @@ function renderTitle() {
   ctx.globalAlpha = 1;
   ctx.fillStyle = "white";
   ctx.fillRect(0,0,canvas.width,canvas.height);
-  var image = document.getElementById("image");
-  ctx.drawImage(image,0,0,canvas.width,canvas.height);
-  ctx.globalAlpha = 0.5;
-  ctx.fillStyle = "#000099";
-  ctx.fillRect(0,0,canvas.width,canvas.height);
+  if ( titleMode == 0 ) {
+    var image = document.getElementById("image");
+    ctx.drawImage(image,0,0,canvas.width,canvas.height);
+    ctx.globalAlpha = 0.5;
+    ctx.fillStyle = "#000099";
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+  }
   ctx.globalAlpha = 1;
   ctx.font = canvas.width * 0.125 + "px Menlo";
   ctx.strokeStyle = "white";
@@ -26,18 +28,19 @@ function renderTitle() {
     ctx.fillText("Pick a character to play as:",canvas.width * 0.5,canvas.height * 0.5);
     ctx.fillText(Math.abs(titleCountryX - Math.round(titleCountryX)) < 0.01 ? names[Math.round(titleCountryX)] : "",canvas.width * 0.5,canvas.height * 0.5375);
   }
-  ctx.strokeStyle = "white";
   var radius = canvas.width * 0.11;
   for ( var i = 0; i < names.length; i++ ) {
     if ( names[i] == "Switzerland" ) continue;
+    ctx.strokeStyle = "black";
     ctx.save();
     ctx.beginPath();
     ctx.arc(canvas.width * 0.5 + radius * 2.5 * (i - titleCountryX),canvas.height * 0.7,radius,0,2 * Math.PI);
+    if ( titleMode == 1 ) ctx.stroke();
     ctx.clip();
     for ( var j = 0; j < flags[i].length; j++ ) {
       var pos = [Math.floor(j / 3),j % 3];
       var pixelPosition = [(pos[0] - 1.5) * (radius / 1.5),(pos[1] - 1.5) * (radius / 1.5)];
-      ctx.globalAlpha = Math.abs(titleCountryX - i) < 0.99 ? 1 : 0.5;
+      if ( titleMode == 0 ) ctx.globalAlpha = Math.abs(titleCountryX - i) < 0.99 ? 1 : 0.5;
       ctx.fillStyle = ["red","orange","yellow","green","blue","purple","black","white"][flags[i][j]];
       ctx.fillRect(canvas.width * 0.5 + radius * 2.5 * (i - titleCountryX) + pixelPosition[0],canvas.height * 0.7 + pixelPosition[1],radius / 1.5,radius / 1.5);
     }
